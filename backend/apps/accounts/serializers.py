c974fad051eacf3a9334f5dc5084e5e13d4a5798
ShekinahCodes <michaelolatunji.dev@gmail.com>
2026-07-29 13:39:20 +0100

    fix: ISE 500

--- backend/apps/accounts/serializers.py
+++ backend/apps/accounts/serializers.py
@@ -7,15 +7,13 @@ User = get_user_model()
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, min_length=6)
+    first_name = serializers.CharField(required=False)
+    last_name = serializers.CharField(required=False)
+    phone = serializers.CharField(required=False)
 
     class Meta:
         model = User
         fields = ("email", "password", "first_name", "last_name", "phone")
-        extra_kwargs = {
-            "first_name": {"required": False},
-            "last_name": {"required": False},
-            "phone": {"required": False},
-        }
 
     def create(self, validated_data):
         first_name = validated_data.pop("first_name", "")
